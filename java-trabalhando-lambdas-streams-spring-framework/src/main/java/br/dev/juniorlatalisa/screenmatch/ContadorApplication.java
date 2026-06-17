b540ee67d75d0ffb765b07d64e325f13dc822a1f
juniorlatalisa <paraojuniorler@gmail.com>
2026-06-17 19:55:50 -0300

    Spring sem Web - ContadorApplication
    Signed-off-by: juniorlatalisa <paraojuniorler@gmail.com>

---
+++ java-trabalhando-lambdas-streams-spring-framework/src/main/java/br/dev/juniorlatalisa/screenmatch/ContadorApplication.java
@@ -0,0 +1,29 @@
+package br.dev.juniorlatalisa.screenmatch;
+
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContadorApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContadorApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Bem vindo ao contador!");
+        System.out.println("----------------------");
+        System.out.println("Digite um número para contagem: ");
+        final int numero;
+        try (final var entrada = new Scanner(System.in)) {
+            numero = entrada.nextInt();
+        }
+        for (int i = 1; i <= numero; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
